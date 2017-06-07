f7b7a101ae3813018b8b4de0469277ee43bf5b8b
Alexander Wallin <office@alexanderwallin.com>
2017-06-07 02:00:09 +0100

    Improves debugging with --dry and --verbose options

--- src/cli.js
+++ src/cli.js
@@ -2,9 +2,21 @@ import parseArgs from 'minimist'
 
 import guessId3 from './index.js'
 
-const args = parseArgs(process.argv.slice(2))
+const args = parseArgs(process.argv.slice(2), {
+  boolean: ['dry', 'verbose'],
+})
 const globs = args._
 
-guessId3(globs)
+if (args.help || args.h) {
+  console.log('guess-id3 [--dry] [--verbose] <pattern> [<pattern> ...]')
+  process.exit(0)
+}
+
+const options = {
+  dry: args.dry === true,
+  verbose: args.verbose === true,
+}
+
+guessId3(globs, options)
   .then(() => console.log('Done.'))
   .catch(err => console.error(err))
--- src/index.js
+++ src/index.js
@@ -3,23 +3,43 @@ import path from 'path'
 import glob from 'glob-all'
 import ID3Writer from 'browser-id3-writer'
 
-export default function guessId3(filenamePattern) {
+const defaultOpts = {
+  dry: false,
+  verbose: false,
+}
+
+export default function guessId3(filenamePattern, options) {
+  const mergedOpts = Object.assign(defaultOpts, options)
   const files = glob.sync(filenamePattern)
+
+  if (mergedOpts.verbose === true) {
+    console.log('Matched files:', files)
+  }
 
   return Promise.all(
     files.map(filename => {
       return new Promise((resolve, reject) => {
         const { artist, title } = extractSongInfoFromFilename(filename)
 
-        const fileContents = fs.readFileSync(filename)
+        if (mergedOpts.dry === true) {
+          console.log(filename, '->', { artist, title })
+        } else {
+          const fileContents = fs.readFileSync(filename)
 
-        const writer = new ID3Writer(fileContents)
-        writer.setFrame('TIT2', title)
-        writer.setFrame('TPE2', artist)
-        writer.addTag()
+          if (mergedOpts.verbose === true) {
+            console.log('Writing follow tag to', filename, ':')
+            console.log('  TIT2 (song title):', title)
+            console.log('  TPE2 (artist name):', artist)
+          }
 
-        const taggedSongBuffer = Buffer.from(writer.arrayBuffer)
-        fs.writeFileSync(filename, taggedSongBuffer)
+          const writer = new ID3Writer(fileContents)
+          writer.setFrame('TIT2', title)
+          writer.setFrame('TPE2', artist)
+          writer.addTag()
+
+          const taggedSongBuffer = Buffer.from(writer.arrayBuffer)
+          fs.writeFileSync(filename, taggedSongBuffer)
+        }
 
         resolve()
       })
